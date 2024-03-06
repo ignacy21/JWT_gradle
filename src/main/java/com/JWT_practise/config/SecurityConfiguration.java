1af9e15eacdfc1fe7cb02dd12f8650a7ef49f2bf
ignacyszpilowski <igancyszpilowski@gmial.com>
2024-03-06 23:45:20 +0100

    new config

--- src/main/java/com/JWT_practise/config/SecurityConfiguration.java
+++ src/main/java/com/JWT_practise/config/SecurityConfiguration.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,25 +22,44 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+//        http
 //                .cors()
 //                .and()
-                .csrf()
-                .disable()
-//                .authorizeHttpRequests().requestMatchers("/api/v1/auth/**")
-                .authorizeHttpRequests().requestMatchers("/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+//                .csrf()
+//                .disable()
+////                .authorizeHttpRequests()
+////                .requestMatchers("/**")
+//                .authorizeHttpRequests()
+//                .requestMatchers("/api/v1/auth/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        DefaultSecurityFilterChain build = http
+                .authorizeHttpRequests(auth -> {
+//                    auth.requestMatchers("/api/v1/auth/**").permitAll();
+                    auth.requestMatchers("/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .csrf(csrf -> {
+                    csrf.ignoringRequestMatchers("/**");
+                })
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
 
-        return http.build();
+//        return http.build();
+        return build;
 
 
     }
